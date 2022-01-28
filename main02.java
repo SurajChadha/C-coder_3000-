4184293baac7b324d133d77eb78d74d4b58d46b3
Suraj Chadha <84652747+SurajChadha@users.noreply.github.com>
2022-01-28 08:45:34 +0000

    Java Average code

---
+++ main02.java
@@ -0,0 +1,16 @@
+public class Program
+{
+    int ar[];
+    ar = new int[];
+    class average
+    {
+         public static void main(String[] args) {
+        for(i=0;i<=n;i++)
+        {
+              sum = arr[sum];
+              c = sum/n;
+              n++;     
+        }
+        System.out.println("The average is: ",c);
+    }
+  }
