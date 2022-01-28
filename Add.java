84ddd5cca8c5fa4f932eb4ad714b1628bec4dc40
Suraj Chadha <84652747+SurajChadha@users.noreply.github.com>
2022-01-28 13:23:49 +0000

    Adding two numbers

---
+++ Add.java
@@ -0,0 +1,20 @@
+public class Program
+{
+int a,b;
+    public static void main(String[] args) {
+    
+    
+     
+     Program
+     {
+     
+     p1.a = a;
+     p1.b = b;
+     c = a + b;
+     
+         
+     }
+     System.out.println("The sum is :- ",p1.c);
+        
+    }
+}
