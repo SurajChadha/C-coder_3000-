7e3ddc3e28e18d315d1c5317150366602cb65407
Suraj Chadha <84652747+SurajChadha@users.noreply.github.com>
2022-02-02 08:36:17 +0000

    Room java program needs help in errors resolving

---
+++ Room_2.java
@@ -0,0 +1,32 @@
+public class Program
+{
+    public class Room_Array{
+        get()
+        {
+            int l1,b1,a1,p1;
+            static int l1 = new Room[l1];
+            static int b1 = new Room[b1];
+            static int a1 = new Room[a1];
+            static int p1 = new Room[p1];
+            
+        }
+        area()
+        {
+            a1 = l1×b1;
+            System.out.println("The area is"+a1);
+        }
+        peri()
+        {
+            p1 = 2(l1+b1);
+            System.out.println("The peri    is:"+p1);
+        }
+        public static void main(String ar[])
+        {
+            System.out.println("The Area is"+a1+"The perimeter is"+p1); 
+        }
+        
+        
+    }
+        
+    }
+}
