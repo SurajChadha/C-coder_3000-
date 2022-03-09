07bf0eec54f27f11fabf9e3bcf08b2446ea2c706
SurajChadha <84652747+SurajChadha@users.noreply.github.com>
2022-03-09 14:07:21 +0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,24 @@
+public class MyClass 
+     {
+         Scanner sc=next Scanner(System.in);
+         int number,diff;
+         String name;
+         void get(){
+         System.out.println("Get all values");
+         sc.get();
+         sc.display();
+         sc.nextInt();
+         sc.nextString();
+             
+         }
+     }
+    public static void main(String args[]) {
+		for(i=0;i<=number;i++)
+            {
+              while(len>=number)
+		      {
+                System.out.println("New String");
+		      }
+            }
+    }
+}
